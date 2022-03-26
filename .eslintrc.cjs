3d0aa1e88d6ef5692be8b89375d640ec0ab1f182
Thorsten Luenborg <t.luenborg@googlemail.com>
2022-03-26 16:10:12 +0100

    add eslint-config-airbnb & vue-accessibility
    This brings the config on par with `@vue/eslint-config-airbnb`,
    expect for the webpack-import resolvers.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,4 +1,5 @@
 /* eslint-env node */
+/* eslint-disable import/no-extraneous-dependencies */
 require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
@@ -7,9 +8,14 @@ module.exports = {
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-typescript/recommended',
+    'airbnb-base',
+    'plugin:vuejs-accessibility/recommended',
     '@vue/eslint-config-prettier',
   ],
   env: {
     'vue/setup-compiler-macros': true,
+  },
+  rules: {
+    'vuejs-accessibility/no-onchange': ['off'],
   },
 }
